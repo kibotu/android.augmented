4e1813dafb9a8008ec33d144aab3952fee27f29f
Apu <A.O.Schilling@GMX.de>
2013-02-07 18:52:47 +0100

    project setup

---
+++ src/net/kibotu/android/albert/ViWiTraMain.java
@@ -0,0 +1,11 @@
+package net.kibotu.android.albert;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Apu
+ * Date: 07.02.13
+ * Time: 17:56
+ * To change this template use File | Settings | File Templates.
+ */
+public class ViWiTraMain {
+}
